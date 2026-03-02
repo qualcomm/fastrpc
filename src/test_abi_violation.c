fdbbdcd3b45049d777eaa9a66f55a2f82797f940
Anand Kulkarni <anankulk@qti.qualcomm.com>
2026-03-02 11:48:55 +0530

    CI : Test change to validate ABI_checker
    Signed-off-by: Anand Kulkarni <anankulk@qti.qualcomm.com>

---
+++ src/test_abi_violation.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* 
+ * Dummy function for testing CI workflow.
+ * This file is intended to test if the ABI checker runs.
+ */
+void test_abi_logic_execution(void) {
+    printf("This is a test artifact for ABI validation.\n");
+}
